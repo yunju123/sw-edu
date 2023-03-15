3051cf278b5e307bc7ccec2964972cd19b71caf9
10150835 <joohyun.yoon@kt.com>
2023-03-15 23:37:34 +0900

    Spring Cloud Sample Code 추가

--- src/main/java/com/kt/sw/edu/api/friend/FriendPropertiesController.java
+++ src/main/java/com/kt/sw/edu/api/friend/FriendPropertiesController.java
@@ -1,4 +1,4 @@
-package com.kt.sw.edu.friend;
+package com.kt.sw.edu.api.friend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/kt/sw/edu/api/friend/MyPropertiesController.java
+++ src/main/java/com/kt/sw/edu/api/friend/MyPropertiesController.java
@@ -1,10 +1,12 @@
-package com.kt.sw.edu.friend;
+package com.kt.sw.edu.api.friend;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/properties")
 public class MyPropertiesController {
